94b2425e8ffcab95d3dcc98c6cf064f6776da2dd
DavidGomezOv <david182400@gmail.com>
2023-02-25 11:26:10 -0500

    Added constant file to store the strings in the application

---
+++ lib/src/core/constants/constants.dart
@@ -0,0 +1,9 @@
+extension Messages on String {
+  static const appTitle = 'Omnipro Test';
+  static const noInternetConnection = 'The device is not connected to Internet';
+  static const statusCodeMessage = 'Status code ';
+}
+
+extension Endpoints on String {
+  static const baseUrl = 'https://jsonplaceholder.typicode.com/photos';
+}
